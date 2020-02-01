138e376e754c61e6d5f2846e7c956535a59bbc6d
root <root@localhost.localdomain>
2020-02-01 09:46:22 -0500

    add source code install and run

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo "Start Install with root"
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm install -g is-website-vulnerable
+echo "Install done !"
---
+++ run.sh
@@ -0,0 +1,9 @@
+echo "=========================="
+echo "=    code by Shaco JX    ="
+echo "=    SJX Known Vulns     ="
+echo "=   fast find vulns web  ="
+echo "=       SKV              ="
+echo "=========================="
+echo -n "Enter your website: (ex: https://site.com/) "
+read website
+npx is-website-vulnerable $website
